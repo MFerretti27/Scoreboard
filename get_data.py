84637af5a89316eecda3189b2f396d61351c9277
Matt Ferretti <Ferretti7@yahoo.com>
2024-12-24 17:44:13 -0500

    Removed betting stats

--- get_data.py
+++ get_data.py
@@ -61,12 +61,6 @@ def get_data(URL: str, team: str, sport: str) -> list:
             # Check if Game hasn't been played yet
             elif not currently_playing:
                 team_info['bottom_info'] = str(team_info['bottom_info'] + "@ " + venue)
-                overUnder = (competition["odds"][0]["overUnder"])
-                spread = (competition["odds"][0]["details"])
-                if "NHL" in URL.upper():
-                    team_info['top_info'] = f"MoneyLine: {spread} \t OverUnder: {overUnder}"
-                else:
-                    team_info['top_info'] = f"Spread: {spread} \t OverUnder: {overUnder}"
 
             # If looking at NFL team get this data (only if currently playing)
             if "NFL" in URL.upper() and currently_playing:
