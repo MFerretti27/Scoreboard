e4da5dcd715030dafde7f48e888fedbffececd16
Matt Ferretti <Ferretti7@yahoo.com>
2025-04-09 16:47:58 -0400

    Removed unused timers

--- currently_playing.py
+++ currently_playing.py
@@ -22,9 +22,7 @@ def team_currently_playing(window: sg.Window, teams: list, SPORT_URLS) -> list:
     stay_on_team = False
 
     display_clock = ticks_ms()  # Start timer for switching display
-    fetch_clock = ticks_ms()  # Start timer for switching display
     display_timer = 25 * 1000  # How often the display should update in seconds
-    fetch_timer = 25 * 1000  # How to fetch for data in seconds
     event = window.read(timeout=5000)
 
     while True in teams_currently_playing or first_time:
