89ac10fc8da51dff77844664b8f4dd56660dfe3d
mferretti27 <ferretti7@yahoo.com>
2025-08-09 15:03:40 -0400

    made order listbox smaller

--- gui_layouts/reorder_teams_layout.py
+++ gui_layouts/reorder_teams_layout.py
@@ -23,7 +23,7 @@ def create_order_teams_layout(window_width: int) -> list:
     button_size = min(max_size, max(38, int(40 * scale)))
     list_box_size_height = min(max_size, max(10, int(15 * scale)))
     list_box_size_width = min(max_size, max(18, int(40 * scale)))
-    list_box_txt_size = min(max_size, max(18, int(24 * scale)))
+    list_box_txt_size = min(max_size, max(18, int(22 * scale)))
     message_size = min(max_size, max(6, int(22 * scale)))
     move_button_size = min(max_size, max(38, int(20 * scale)))
 
